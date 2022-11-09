55e67f02b937349491b21c388a92766fe8326d56
Paulfresnel <paul.fresnel1@gmail.com>
2022-11-09 19:31:55 +0100

    Drivers comparator page started
    API limit reached, wait 5h30 until daily reset to continue working

--- app.js
+++ app.js
@@ -30,13 +30,33 @@ app.get('/drivers', (req,res)=>{
         }
       };
       axios(config)
-      .then(function (response) {
+      .then((response) => {
         console.log(response.data);
+        res.render('drivers')
       })
       .catch(function (error) {
         console.log(error);
       });
-    res.render('drivers')
+    
+})
+
+
+app.get('/search', (req,res)=>{
+  let driver1, driver2;
+  console.log(req.query)
+  axios.get(`https://v1.formula-1.api-sports.io/drivers?search=${req.query.driver1}`, {headers: {'x-rapidapi-key': 'eadc164ca7c36b540c39625b8e3f2384','x-rapidapi-host': 'v1.formula-1.api-sports.io'}})
+  .then((data) => {
+    return driver1 = data.data.response
+  })
+  .then ((driver1)=>{
+    axios.get(`https://v1.formula-1.api-sports.io/drivers?search=${req.query.driver2}`, {headers: {'x-rapidapi-key': 'eadc164ca7c36b540c39625b8e3f2384','x-rapidapi-host': 'v1.formula-1.api-sports.io'}})
+  .then((data)=>{
+    driver2 = data.data.response
+    console.log(driver1)
+    console.log(driver2)
+    res.render('driver', {driver1, driver2})
+  })
+  })   
 })
 
 app.listen(PORT, ()=> console.log(`App is running on port ${PORT}`))
