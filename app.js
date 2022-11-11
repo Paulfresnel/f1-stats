b7c5267b9e632015878a9bf210cbb9b50aa7c675
Paulfresnel <paul.fresnel1@gmail.com>
2022-11-11 18:42:28 +0100

    styling and dynamic values

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.get('/drivers', (req,res)=>{
     
 })
 
+let statsDiff = [{}];
 
 app.get('/search', (req,res)=>{
   let driver1, driver2;
@@ -52,9 +53,27 @@ app.get('/search', (req,res)=>{
     axios.get(`https://v1.formula-1.api-sports.io/drivers?search=${req.query.driver2}`, {headers: {'x-rapidapi-key': 'eadc164ca7c36b540c39625b8e3f2384','x-rapidapi-host': 'v1.formula-1.api-sports.io'}})
   .then((data)=>{
     driver2 = data.data.response
-    console.log(driver1)
-    console.log(driver2)
-    res.render('driver', {driver1, driver2})
+    let driver1Points = parseInt(driver1[0].career_points)
+    let driver2Points = parseInt(driver2[0].career_points)
+    statsDiff[0].points =  driver1Points - driver2Points
+    statsDiff[0].grands_prix_entered =  driver1[0].grands_prix_entered - driver2[0].grands_prix_entered
+    statsDiff[0].podiums = driver1[0].podiums - driver2[0].podiums
+    statsDiff[0].championships = driver1[0].world_championships - driver2[0].world_championships
+    
+
+    driver1[0].podium_percentage = ((driver1[0].podiums/driver1[0].grands_prix_entered)*100).toFixed(2)
+    driver2[0].podium_percentage = ((driver2[0].podiums/driver2[0].grands_prix_entered)*100).toFixed(2)
+    statsDiff[0].percentage = (((driver1[0].podium_percentage - driver2[0].podium_percentage)/driver2[0].podium_percentage)*100).toFixed(2)
+
+    //if (statsDiff[0].points >=0){
+    //  window.getElementById('stats-dom').style.color = "green"
+    //}
+    //if (statsDiff[0].points <=0){
+    //  window.getElementById('stats-dom').style.color = "red"
+    //} 
+    //console.log(driver1)
+    //console.log(driver2)
+    res.render('driver', {driver1, driver2, statsDiff})
   })
   })   
 })
